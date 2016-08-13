bdd192b9cf95019f8b6cbc4a4d38c52a3722fc01
Ross Fletcher <rossf97@outlook.com>
2016-08-13 19:29:47 +0100

    Comments on login

--- src/Employee.java
+++ src/Employee.java
@@ -1,8 +1,19 @@
+/**
+ * class Employee extends SearchableObject
+ *
+ * This is a blueprint for the employee object that will be constucted in
+ * the main and used in an ArrayList of Employees. This extends searchable
+ * object so that it can be user by the search classes
+ *
+ */
 class Employee extends SearchableObject {
     private String firstName, lastName, department, userName, password;
     private double salary;
 
+    // This is the constuctor for the employee class this will create the employee object
     public Employee(String firstName, String lastName, String department, String userName, String password, double salary) {
+        // The name attribute is constructed by concatenating the first and list names
+        // The type attibutes it set as employee
         super(firstName + " " + lastName, Type.EMPLOYEE);
         this.firstName = firstName;
         this.lastName = lastName;
@@ -12,52 +23,29 @@ class Employee extends SearchableObject {
         this.salary = salary;
     }
 
+    // These are the getters for the class that allow the getting of individual attributes
     public String getFirstName() {
         return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
     }
 
     public String getLastName() {
         return lastName;
     }
 
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
     public String getDepartment() {
         return department;
-    }
-
-    public void setDepartment(String department) {
-        this.department = department;
     }
 
     public String getUserName() {
         return userName;
     }
 
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
     public String getPassword() {
         return password;
     }
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
     public double getSalary() {
         return salary;
-    }
-
-    public void setSalary(double salary) {
-        this.salary = salary;
     }
 }
 
